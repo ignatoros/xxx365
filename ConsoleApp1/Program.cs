f4e97bd311a3f2d7c8350483a4e305884b3138a1
yaroshevski <wsr1@K218PC1>
2022-04-11 13:19:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿ using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            double b = 2;
+            var c = 32;
+
+        }
+    }
+}
